d859aa3b475648de3ff113d43d901d0f2e2db3c9
Abima <abimael.santa1@gmail.com>
2024-02-18 15:38:34 -0400

    Enable Cors config

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-        origin: "https://abda-ecomerce-frontend-bigk-git-abimael-abimael-santas-projects.vercel.app",
+        origin: true,
         credentials: true,
-        methods: "POST, GET, DELETE, PUT, OPTIONS",
-        allowedHeaders: "POST, GET, DELETE, PUT, OPTIONS", 
-        preflightContinue: true,
+        methods: ["POST", "GET", "DELETE", "PUT"],
+        allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'Authorization'], 
+        exposedHeaders: ['Authorization']
         
   });
   await app.listen(3000);
