d1f8689a86a5709f9a742e39a8b4a0ef095c031b
Abima <abimael.santa1@gmail.com>
2024-02-18 15:44:12 -0400

    url added

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-        origin: true,
+        origin: "https://abda-ecomerce-frontend-bigk-git-abimael-abimael-santas-projects.vercel.app",
         credentials: true,
         methods: ["POST", "GET", "DELETE", "PUT"],
         allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'Authorization'], 
