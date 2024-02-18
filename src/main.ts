ef9e99d8e441ec71f78fa587ad84aaf63cfaf4fd
Abima <abimael.santa1@gmail.com>
2024-02-18 10:31:15 -0400

    cors error

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
         origin: process.env.CORS_ALLOWED_ORIGIN,
+        methods: "Get, Post, , Put, Delete",
+        allowedHeaders: "*", 
         preflightContinue: true,
   });
   await app.listen(3000);
