c868b6c609afbc37b17c20bbcb5b10fa81c65c35
Abima <abimael.santa1@gmail.com>
2024-02-18 10:40:09 -0400

    cors credentials

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-        origin: process.env.CORS_ALLOWED_ORIGIN,
+        origin: process.env.CORS_ALLOWED_ORIGIN || '*',
+        credentials: true,
         methods: "Get, Post, , Put, Delete",
         allowedHeaders: "*", 
         preflightContinue: true,
