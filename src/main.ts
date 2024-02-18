e7af6416a35c0c3ff247fdf5cf44ea6abe56f935
Abima <abimael.santa1@gmail.com>
2024-02-18 15:29:13 -0400

    Cors Headers

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-        origin: "http://localhost:3000" || '*',
+        origin: "http://localhost:3000",
         credentials: true,
-        methods: "Get, Post, , Put, Delete, Options",
-        allowedHeaders: "*", 
+        methods: "POST, GET, DELETE, PUT, OPTIONS",
+        allowedHeaders: "POST, GET, DELETE, PUT, OPTIONS", 
         preflightContinue: true,
         
   });
