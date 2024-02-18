3e907b406685829b7d6d40a6f16732652d37741a
Abima <abimael.santa1@gmail.com>
2024-02-18 00:19:03 -0400

    Prisma Module

--- src/user/User.Module.ts
+++ src/user/User.Module.ts
@@ -1,7 +1,7 @@
 import { Module } from "@nestjs/common";
 import { UserController } from "./User.Controller";
 import { UserService } from "./User.Service";
-import {PrismaModule} from "src/Prisma/prisma.module"
+import { PrismaModule } from "src/prisma/prisma.module";
 @Module({
     controllers:[UserController],
     providers: [UserService],
