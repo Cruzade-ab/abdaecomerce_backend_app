b0d4d76188b95538b11c51c470ac2ada67aff67c
Abima <abimael.santa1@gmail.com>
2024-02-18 18:10:08 -0400

    Importing user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
-
+import {UserModule} from './user/User.Module'
 
 @Module({
-  imports: [],
+  imports: [UserModule],
   controllers: [],
   providers: [],
 })
