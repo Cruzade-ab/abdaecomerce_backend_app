c668f3902fd363171df5beadbd55f9704edf2b91
Abima <abimael.santa1@gmail.com>
2024-02-18 16:11:11 -0400

    User.Controller

--- src/user/User.Controller.ts
+++ src/user/User.Controller.ts
@@ -1,26 +1,16 @@
-import { Body, Controller, Get, Post, Delete, Param } from "@nestjs/common";
+
+import { Body, Controller, Post } from "@nestjs/common";
 import { UserService } from "./User.Service";
 import { User } from "@prisma/client";
 
-@Controller('https://backendapp-production-5383.up.railway.app/api/submit-form')
-export class UserController{
+@Controller('/api/submit-form') 
+export class UserController {
 
-    constructor(private readonly userService: UserService){}
+    constructor(private readonly userService: UserService) {}
 
-    @Post()//Decorador
+    @Post()
     async createUser(@Body() data: User) {
-        return this.userService.createUser(data)
+        return this.userService.createUser(data);
     }
 
-    @Get()
-    async getAllUsers(){
-        return this.userService.getAllUsers()
-    }
-
-    @Delete(':id')
-    async deleteUser(@Param(':id') id:string ){
-        return this.userService
-    }
-//@Delete(':id')
-//async deleteTask(@Param('id') id:string) {
 }
