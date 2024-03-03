e47ddda1aea2dd6f6a7b6147fcfe0beedd71c598
DairanDoo <CorreoTemporal@gmail.com>
2024-03-03 17:03:09 -0400

    Implementamos al funcionalidad del login, register, logout y getUser con byCrypt para encriptar el password y JWT para crear el token de sesión a través de una cookie para guardar esa sesión.

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
 
 const port = process.env.PORT || 3000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
   app.enableCors({
         origin: "https://abda-ecomerce-frontend-bigk-git-abimael-abimael-santas-projects.vercel.app",
         credentials: true,
---
+++ src/user/user.controller.ts
@@ -0,0 +1,35 @@
+import { Controller, Injectable, Post, Body, Res, Get, Req} from '@nestjs/common';
+import { UserService } from './user.service';
+import { User } from '@prisma/client';
+import { Response, Request } from 'express';
+
+@Injectable()
+@Controller('api/user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  // crear usuario
+  @Post('register')
+  async createUser(@Body() data: User) {
+	return this.userService.createUser(data);
+  }
+
+  // logear usuario
+  @Post('login')
+    async logUser(@Body() data: User, @Res({passthrough: true}) res: Response) {
+        return this.userService.logUser(data, res);
+    }
+
+	// Obtener usuario del token
+	@Get('getUser')
+    async getUser(@Req() request: Request) {
+        return this.userService.getUserFromToken(request);
+    }
+
+    // Hacer logout del usuario.
+    @Post('logout')
+    async logoutUser(@Res({passthrough: true}) response: Response) {
+        return this.userService.logoutUser(response);
+    }
+
+}
