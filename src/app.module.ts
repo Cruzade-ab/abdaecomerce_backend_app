4687c4abd789f691bfe3f9c7181ea9d4501a31c9
Abima <abimael.santa1@gmail.com>
2024-03-03 14:23:31 -0400

    Limpieza para empezar de cero

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
-import {RegisterModule} from './register/Register.Module'
+
 
 @Module({
-  imports: [RegisterModule],
+  imports: [],
   controllers: [],
   providers: [],
 })
