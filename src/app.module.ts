5f4c63a3e2f498f9cca29ac93acb735412244b2a
Abima <abimael.santa1@gmail.com>
2024-03-29 17:45:13 -0400

    .

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,17 +4,10 @@ import { ProductsModule } from './products/products.module';
 import { AdminModule } from './admin/admin.module';
 import { CloudinaryModule } from './cloudinary/cloudinary.module';
 import { PrismaModule } from './prisma/prisma.module';
-import { UserController } from './user/user.controller';
-import { AdminController } from './admin/admin.controller';
-import { ProductsController } from './products/products.controller';
-import { UserService } from './user/user.service';
-import { AdminService } from './admin/admin.service';
-import { ProductsService } from './products/products.service';
+
 
 
 @Module({
   imports: [UserModule, ProductsModule, AdminModule, CloudinaryModule, PrismaModule],
-  controllers: [UserController, AdminController, ProductsController, ],
-  providers: [ UserService, AdminService, ProductsService],
 })
 export class AppModule {}
