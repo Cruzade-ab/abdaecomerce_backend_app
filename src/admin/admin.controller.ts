90ed745782e8dfdcf8c19264a7966887c316f467
Abima <abimael.santa1@gmail.com>
2024-03-29 18:16:02 -0400

    Creacion De producto Exitosa

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,6 +1,6 @@
-import { Injectable, Controller, Post, UseInterceptors, Body, UploadedFiles } from "@nestjs/common";
+import { Injectable, Controller, Post, UseInterceptors, Body, UploadedFiles, } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
-import { FilesInterceptor } from "@nestjs/platform-express";
+import { AnyFilesInterceptor } from "@nestjs/platform-express";
 import { ProductRecived } from "src/dto/product_recived";
 import { AdminService } from "./admin.service";
 
@@ -10,13 +10,14 @@ export class AdminController {
     constructor(
         private readonly prisma: PrismaService,
         private readonly adminService: AdminService
-        ) {}
+    ) {}
 
     @Post('/product/create')
-    @UseInterceptors(FilesInterceptor('files'))
+    @UseInterceptors(AnyFilesInterceptor())
     async createProduct(
         @Body() product: ProductRecived,
-        @UploadedFiles() files: Express.Multer.File[],): Promise<ProductRecived> {
+        @UploadedFiles() files: Express.Multer.File[],
+    ): Promise<ProductRecived> {
         console.log('Received product:', product);
         console.log('Received files:', files);
         console.log('Entering createProduct method');
